zefie/kernel_moto_shamu

Add build script

build.sh
@@ -1,0 +1,167 @@
+#!/bin/bash
+
+# Bash Color
+green='\033[01;32m'
+red='\033[01;31m'
+blink_red='\033[05;31m'
+restore='\033[0m'
+
+clear
+
+# Resources
+THREAD="-j9"
+KERNEL="zImage-dtb"
+DEFCONFIG="hells_defconfig"
+
+# Kernel Details
+BASE_HC_VER="hC"
+VER="-b1-t1-M2"
+HC_VER="$BASE_HC_VER$VER"
+
+# Vars
+export LOCALVERSION=-`echo $HC_VER`
+export ARCH=arm
+export SUBARCH=arm
+
+# Paths
+KERNEL_DIR=`pwd`
+REPACK_DIR="${HOME}/Android/Kernel/hC-N6-anykernel-M"
+ZIP_MOVE="${HOME}/Android/Kernel/hC-releases/N6"
+ZIMAGE_DIR="${HOME}/Android/Kernel/hells-Core-N6/arch/arm/boot"
+DB_FOLDER="${HOME}/Dropbox/Kernel-Betas/N6"
+
+# Functions
+function clean_all {
+		rm -rf $REPACK_DIR/kernel/zImage
+		rm -rf $ZIMAGE_DIR/$KERNEL
+		make clean && make mrproper
+}
+
+function make_kernel {
+		make $DEFCONFIG
+		make $THREAD
+		cp -vr $ZIMAGE_DIR/$KERNEL $REPACK_DIR/kernel
+		mv ${HOME}/Android/Kernel/hC-N6-anykernel-M/kernel/zImage-dtb ${HOME}/Android/Kernel/hC-N6-anykernel-M/kernel/zImage
+}
+
+function make_zip {
+		cd $REPACK_DIR
+		zip -9 -r `echo $HC_VER`.zip .
+		mv  `echo $HC_VER`.zip $ZIP_MOVE
+		cd $KERNEL_DIR
+}
+
+function copy_dropbox {
+		cd $ZIP_MOVE
+		cp -vr  `echo $HC_VER`.zip $DB_FOLDER
+		cd $KERNEL_DIR
+}
+
+DATE_START=$(date +"%s")
+
+echo -e "${green}"
+echo "hC Kernel Creation Script:"
+echo
+
+echo "---------------"
+echo "Kernel Version:"
+echo "---------------"
+
+echo -e "${red}"; echo -e "${blink_red}"; echo "$HC_VER"; echo -e "${restore}";
+
+echo -e "${green}"
+echo "-----------------"
+echo "Making hC Kernel:"
+echo "-----------------"
+echo -e "${restore}"
+
+while read -p "Please choose your option: [1]clean-build / [2]dirty-build / [3]zip-only / [4]abort " cchoice
+do
+case "$cchoice" in
+	1 )
+		echo -e "${green}"
+		echo
+		echo "[..........Cleaning up..........]"
+		echo
+		echo -e "${restore}"
+		clean_all
+		echo -e "${green}"
+		echo
+		echo "[....Building `echo $HC_VER`....]"
+		echo
+		echo -e "${restore}"
+		make_kernel
+		echo -e "${green}"
+		echo
+		echo "[....Make `echo $HC_VER`.zip....]"
+		echo
+		echo -e "${restore}"
+		make_zip
+		echo -e "${green}"
+		echo
+		echo "[.....Moving `echo $HC_VER`.....]"
+		echo
+		echo -e "${restore}"
+		copy_dropbox
+		break
+		;;
+	2 )
+		echo -e "${green}"
+		echo
+		echo "[....Building `echo $HC_VER`....]"
+		echo
+		echo -e "${restore}"
+		make_kernel
+		echo -e "${green}"
+		echo
+		echo "[....Make `echo $HC_VER`.zip....]"
+		echo
+		echo -e "${restore}"
+		make_zip
+		echo -e "${green}"
+		echo
+		echo "[.....Moving `echo $HC_VER`.....]"
+		echo
+		echo -e "${restore}"
+		copy_dropbox
+		break
+		;;
+	3 )
+		echo -e "${green}"
+		echo
+		echo "[....Make `echo $HC_VER`.zip....]"
+		echo
+		echo -e "${restore}"
+		make_zip
+		echo -e "${green}"
+		echo
+		echo "[.....Moving `echo $HC_VER`.....]"
+		echo
+		echo -e "${restore}"
+		copy_dropbox
+		break
+		;;
+	4 )
+		break
+		;;
+	* )
+		echo -e "${red}"
+		echo
+		echo "Invalid try again!"
+		echo
+		echo -e "${restore}"
+		;;
+esac
+done
+
+echo -e "${green}"
+echo "-------------------"
+echo "Build Completed in:"
+echo "-------------------"
+echo -e "${restore}"
+
+DATE_END=$(date +"%s")
+DIFF=$(($DATE_END - $DATE_START))
+echo "Time: $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds."
+echo
+

hellsgod/hells-Core-N6P

build.sh
@@ -1,0 +1,167 @@
+#!/bin/bash
+
+# Bash Color
+green='\033[01;32m'
+red='\033[01;31m'
+blink_red='\033[05;31m'
+restore='\033[0m'
+
+clear
+
+# Resources
+THREAD="-j9"
+KERNEL="Image.gz-dtb"
+DEFCONFIG="hells_defconfig"
+
+# Kernel Details
+BASE_HC_VER="hC"
+VER="-N1"
+HC_VER="$BASE_HC_VER$VER"
+
+# Vars
+export LOCALVERSION=-`echo $HC_VER`
+export ARCH=arm64
+export SUBARCH=arm64
+
+# Paths
+KERNEL_DIR=`pwd`
+REPACK_DIR="${HOME}/Android/Kernel/hC-N6P-anykernel"
+ZIP_MOVE="${HOME}/Android/Kernel/hC-releases/N6P"
+ZIMAGE_DIR="${HOME}/Android/Kernel/hells-Core-N6P/arch/arm64/boot"
+DB_FOLDER="${HOME}/Dropbox/Kernel-Betas/N6P"
+
+# Functions
+function clean_all {
+		rm -rf $REPACK_DIR/kernel/zImage
+		rm -rf $ZIMAGE_DIR/$KERNEL
+		make clean && make mrproper
+}
+
+function make_kernel {
+		make $DEFCONFIG
+		make $THREAD
+		cp -vr $ZIMAGE_DIR/$KERNEL $REPACK_DIR/kernel
+		mv ${HOME}/Android/Kernel/hC-N6P-anykernel/kernel/Image.gz-dtb ${HOME}/Android/Kernel/hC-N6P-anykernel/kernel/zImage
+}
+
+function make_zip {
+		cd $REPACK_DIR
+		zip -9 -r `echo $HC_VER`.zip .
+		mv  `echo $HC_VER`.zip $ZIP_MOVE
+		cd $KERNEL_DIR
+}
+
+function copy_dropbox {
+		cd $ZIP_MOVE
+		cp -vr  `echo $HC_VER`.zip $DB_FOLDER
+		cd $KERNEL_DIR
+}
+
+DATE_START=$(date +"%s")
+
+echo -e "${green}"
+echo "hC Kernel Creation Script:"
+echo
+
+echo "---------------"
+echo "Kernel Version:"
+echo "---------------"
+
+echo -e "${red}"; echo -e "${blink_red}"; echo "$HC_VER"; echo -e "${restore}";
+
+echo -e "${green}"
+echo "-----------------"
+echo "Making hC Kernel:"
+echo "-----------------"
+echo -e "${restore}"
+
+while read -p "Please choose your option: [1]clean-build / [2]dirty-build / [3]zip-only / [4]abort " cchoice
+do
+case "$cchoice" in
+	1 )
+		echo -e "${green}"
+		echo
+		echo "[..........Cleaning up..........]"
+		echo
+		echo -e "${restore}"
+		clean_all
+		echo -e "${green}"
+		echo
+		echo "[....Building `echo $HC_VER`....]"
+		echo
+		echo -e "${restore}"
+		make_kernel
+		echo -e "${green}"
+		echo
+		echo "[....Make `echo $HC_VER`.zip....]"
+		echo
+		echo -e "${restore}"
+		make_zip
+		echo -e "${green}"
+		echo
+		echo "[.....Moving `echo $HC_VER`.....]"
+		echo
+		echo -e "${restore}"
+		copy_dropbox
+		break
+		;;
+	2 )
+		echo -e "${green}"
+		echo
+		echo "[....Building `echo $HC_VER`....]"
+		echo
+		echo -e "${restore}"
+		make_kernel
+		echo -e "${green}"
+		echo
+		echo "[....Make `echo $HC_VER`.zip....]"
+		echo
+		echo -e "${restore}"
+		make_zip
+		echo -e "${green}"
+		echo
+		echo "[.....Moving `echo $HC_VER`.....]"
+		echo
+		echo -e "${restore}"
+		copy_dropbox
+		break
+		;;
+	3 )
+		echo -e "${green}"
+		echo
+		echo "[....Make `echo $HC_VER`.zip....]"
+		echo
+		echo -e "${restore}"
+		make_zip
+		echo -e "${green}"
+		echo
+		echo "[.....Moving `echo $HC_VER`.....]"
+		echo
+		echo -e "${restore}"
+		copy_dropbox
+		break
+		;;
+	4 )
+		break
+		;;
+	* )
+		echo -e "${red}"
+		echo
+		echo "Invalid try again!"
+		echo
+		echo -e "${restore}"
+		;;
+esac
+done
+
+echo -e "${green}"
+echo "-------------------"
+echo "Build Completed in:"
+echo "-------------------"
+echo -e "${restore}"
+
+DATE_END=$(date +"%s")
+DIFF=$(($DATE_END - $DATE_START))
+echo "Time: $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds."
+echo
+